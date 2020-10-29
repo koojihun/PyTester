fe2a2094bc58e18034f64b47ee81df9f1edd4246
Jihoon Koo <koo2788@naver.com>
2020-10-30 00:32:39 +0900

    edit Stock class

--- Stock.py
+++ Stock.py
@@ -1,6 +1,9 @@
 class Stock:
+
     def __init__(self, type, code, name):
-        # TYPE => ( 0 : KOSPI 전체 종목, 1 : KOSDAQ 전체 종목, 2 : KOSPI 특정, 3 : KOSDAQ 특정 )
+
+        # TYPE => ( 0 : KOSPI 특정, 1 : KOSDAQ 특정, 2 : KOSPI 전체, 3 : KOSDAQ 전체 )
         self.type = type
+        
         self.code = code
         self.name = name
