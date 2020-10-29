cb9f714c6de6b29c448883f92b6cbc86b984f988
koojihun <31695651+koojihun@users.noreply.github.com>
2020-10-29 18:44:40 +0900

    Update Target.py

--- left_layout/Target.py
+++ left_layout/Target.py
@@ -1,11 +1,10 @@
-from PyQt5.QtWidgets import QMessageBox
-
 import Market
 from Stock import Stock
 
 class Target:
 
     target_market_list = ["KOSPI 전체", "KOSDAQ 전체", "KOSPI 특정", "KOSDAQ 특정"]
+    search_condition = ["종목 코드", "종목 "]
     err_msg = "결과가 존재하지 않습니다."
 
     def __init__(self, main_window):
@@ -24,6 +23,7 @@ class Target:
         self.target_ok_btn.clicked.connect(self._handle_ok)
 
         self.found = None
+        self.result = None
 
     def _handle_chage_market(self):
         idx = self.target_market_combo_box.currentIndex()
@@ -45,26 +45,28 @@ class Target:
 
         self.found = None
 
-        target_market = self.target_market_combo_box.currentText()
+        target_market_idx = self.target_market_combo_box.currentIndex()
+        search_condition_idx = self.target_search_condition_combo_box.currentIndex()
         key = self.target_search_line_edit.text()
-        search_condition = self.target_search_condition_combo_box.currentText()
 
         if key == "":
             return
 
         # 1. KOSPI 선택
-        if target_market == Target.target_market_list[2]:
-            if search_condition == "종목 코드" and Market.kospi_get_name_by_code(key) is not None:
-                self.found = Stock(key, Market.kospi_get_name_by_code(key))
-            elif search_condition == "종목 이름" and Market.kospi_get_code_by_name(key) is not None:
-                self.found = Stock(Market.kospi_get_code_by_name(key), key)
+        if target_market_idx == 2:
+            # 종목 코드로 검색한 경우.
+            if search_condition_idx == 0 and Market.kospi_get_name_by_code(key) is not None:
+                self.found = Stock(target_market_idx, key, Market.kospi_get_name_by_code(key))
+            # 종목 이름으로 검색한 경우.
+            elif search_condition_idx == 1 and Market.kospi_get_code_by_name(key) is not None:
+                self.found = Stock(target_market_idx, Market.kospi_get_code_by_name(key), key)
 
         # 2. KOSDAQ 선택
         else:
-            if search_condition == "종목 코드" and Market.kospi_get_name_by_code(key) is not None:
-                self.found = Stock(key, Market.kosdaq_get_name_by_code(key))
-            elif search_condition == "종목 이름" and Market.kospi_get_code_by_name(key) is not None:
-                self.found = Stock(Market.kosdaq_get_code_by_name(key), key)
+            if search_condition_idx == 0 and Market.kosdaq_get_name_by_code(key) is not None:
+                self.found = Stock(target_market_idx, key, Market.kosdaq_get_name_by_code(key))
+            elif search_condition_idx == 1 and Market.kosdaq_get_code_by_name(key) is not None:
+                self.found = Stock(target_market_idx, Market.kosdaq_get_code_by_name(key), key)
 
         if self.found is None:
             self.target_search_result_line_edit.setText(Target.err_msg)
@@ -72,11 +74,13 @@ class Target:
             self.target_search_result_line_edit.setText(self.found.code + " " + self.found.name)
 
     def _handle_ok(self):
-        target_market = self.target_market_combo_box.currentText()
-        if target_market == Target.target_market_list[0] or target_market == Target.target_market_list[1]:
-            self.main_window.target_text_label.setText(target_market)
+        target_market_idx = self.target_market_combo_box.currentIndex()
+        if target_market_idx == 0 or target_market_idx == 1:
+            self.result = Stock(target_market_idx, "", "")
+            self.main_window.target_text_label.setText(self.target_market_combo_box.currentText())
             self._switch_search_function(False)
         else:
             if self.found is not None:
+                self.result = Stock(target_market_idx)
                 self.main_window.target_text_label.setText(self.found.code + "\n" + self.found.name)
                 self._switch_search_function(True)
