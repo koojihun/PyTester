0e36c3ef034081039b6b5bf9c7f214b998386227
koojihun <31695651+koojihun@users.noreply.github.com>
2020-10-29 18:29:40 +0900

    Update Stock.py
    Modify Stock class to have type

--- Stock.py
+++ Stock.py
@@ -1,4 +1,6 @@
 class Stock:
-    def __init__(self, code, name):
+    def __init__(self, type, code, name):
+        # TYPE => ( 0 : KOSPI 전체 종목, 1 : KOSDAQ 전체 종목, 2 : KOSPI 특정, 3 : KOSDAQ 특정 )
+        self.type = type
         self.code = code
         self.name = name
